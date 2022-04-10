5ff0d61f7ced07ad48f6a16864e36394ce0f011e
Raihan567 <aburaihansarker567@gmail.com>
2022-04-10 16:53:01 +0600

    create index file

---
+++ Make-Fun-With-Array/index.js
@@ -0,0 +1,25 @@
+const rai = [2, 2, 2, 2, 35, 5, 5];
+console.log(rai);
+
+let hello = [
+  "Raihan",
+  "Sumaiya",
+  "Riya",
+  "Sara",
+  "Gias",
+  "Laden",
+  "NoshimUddin",
+];
+console.log(hello);
+// Constructor pattern
+
+let a1 = new Array(5);
+console.log(a1, a1.length);
+let a2 = new Array(1, 2, 3, 4, 5);
+console.log(a2, a2.length);
+
+// Factory pattern
+let b1 = Array(5);
+console.log(b1, b1.length);
+let b2 = Array(1, 2, 3, 4, 5);
+console.log(b2, b2.length);
