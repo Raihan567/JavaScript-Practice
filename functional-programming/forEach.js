1ec4b739cbefeb84075a8aa09b7262262819762e
Raihan567 <aburaihansarker567@gmail.com>
2022-04-22 21:12:14 +0600

    implement forEach method

--- functional-programming/forEach.js
+++ functional-programming/forEach.js
@@ -9,13 +9,24 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //   );
 // });
 
-function loop(arr, callback) {
+function name(arr, callback) {
   for (let i = 0; i < arr.length; i++) {
     callback(arr[i], i, arr);
   }
 }
-loop(arr, function (element, index, array) {
-  console.log(
-    `Element is = ${element}, index is = ${index} and array is = ${array}`
-  );
+console.log(`--------------Implement ForEach-------------------`);
+name(arr, function (element, index, array) {
+  console.log(`Element = ${element}, index = ${index} and Array = ${array}`);
+});
+console.log("--------------built in ForEach----------------------");
+arr.forEach(function (element, index, array) {
+  console.log(`Element = ${element}, index = ${index} and Array = ${array}`);
+});
+console.log(`----------------Element----------------`);
+arr.forEach((element) => {
+  console.log(`Element = ${element}`);
+});
+
+arr.forEach(function (element, index, array) {
+  console.log(`Element = ${element}, index = ${index} and array = [${array}]`);
 });
