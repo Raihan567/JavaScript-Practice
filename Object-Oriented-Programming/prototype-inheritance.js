d60d9b530b169f1c2e00c977d524d5d2a9b67ebb
Raihan567 <aburaihansarker567@gmail.com>
2022-04-24 08:09:57 +0600

    prototype inheritance

---
+++ Object-Oriented-Programming/prototype-inheritance.js
@@ -0,0 +1,16 @@
+/**
+ * Title: Prototype inheritance
+ */
+
+function Person() {
+  this.name = "Abu Raihan";
+}
+function Teacher() {
+  Person.call(this);
+  console.log(Person.call(this));
+  this.subject = "JavaScript";
+}
+var teacher = new Person();
+var name = teacher.name;
+console.log(name);
+Teacher();
